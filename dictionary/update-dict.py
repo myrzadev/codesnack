6ba7200d3b08b4a76700aaed82527f4629b112e2
Myrza <109143405+myrzadev@users.noreply.github.com>
2022-07-22 09:26:50 +0200

    Update Python dictionary

---
+++ dictionary/update-dict.py
@@ -0,0 +1,9 @@
+# Connect us on Twitter @myrzadev
+
+student = {'name': 'John', 'age': 25, 'courses': 
+['Math', 'CompSci']}
+
+student.update({'name': 'Myrzza', 'age': 35, \
+    'phone': 000-55555})
+
+print(student)
