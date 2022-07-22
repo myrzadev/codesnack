ce3e42943cbf56ca08020f9dbaf00158b3355012
Myrza <109143405+myrzadev@users.noreply.github.com>
2022-07-22 09:34:48 +0200

    Merge two or more dictionaries in python

---
+++ dictionary/merge-dictionaries.py
@@ -0,0 +1,22 @@
+# 1/2
+
+dict_one = {
+    'Argentina': 'Chaco',
+    'Brazil': 'Salvador',
+    'Cyprus': 'Nicosia'}
+
+dict_two = {
+    'Estonia': 'Narva',
+    'Germany': 'Berlin'
+}
+
+print(dict_one)
+print(dict_two)
+
+# 2/2
+# Connect us on Twitter @myrzadev
+
+merged_dict = dict_one.copy()
+merged_dict.update(dict_two)
+
+print(merged_dict)
