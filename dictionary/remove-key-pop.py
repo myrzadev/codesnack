9b2acb7c27ee1d0320a2ccd756fa9a9ffde14cc3
Myrza <109143405+myrzadev@users.noreply.github.com>
2022-07-22 08:19:47 +0200

    Removey key in dictionary

---
+++ dictionary/remove-key-pop.py
@@ -0,0 +1,9 @@
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+
+courses = student.pop('courses')
+
+print(student)
+print(courses)
+
+# {'name': 'John', 'age': 25}
+# ['Math', 'CompSci']
